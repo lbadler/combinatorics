e26dda95b10ed7f717e91fa3674d1ad0a6ec11aa
lbadler <122937810+lbadler@users.noreply.github.com>
2024-03-21 13:25:48 -0400

    added conjugate partitions

--- CombinatoricsStuff.java
+++ CombinatoricsStuff.java
@@ -201,10 +201,21 @@ public class CombinatoricsStuff {
 	
 	//Integer partitions
 	
+	public static int[] generateConjugatePartition (int[] partition) {
+		int[] conjugate = new int[partition[0]];
+		for (int i = 0; i < conjugate.length; i++)
+			for (int j = 0; j < partition.length; j++)
+				if (partition[j] > i)
+					conjugate[i]++;
+		return conjugate;
+	}
 	
 	//Insert my random code to test/output things here
 	public static void main (String[]args) {
-		
+		int[] demoPartition = new int[]{4, 2, 2, 2, 1, 1};
+		int[] demoConjugate = generateConjugatePartition(demoPartition);
+		System.out.println(Arrays.toString(demoPartition));
+		System.out.println(Arrays.toString(demoConjugate));
 	}
 	
 	
