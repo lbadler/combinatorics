bd52f5437f3d3ce377910c523c22c2d8fbbe87ea
lbadler <122937810+lbadler@users.noreply.github.com>
2024-03-25 05:25:07 -0400

    Update PartitionFunctionV2.java

--- PartitionFunctionV2.java
+++ PartitionFunctionV2.java
@@ -1,4 +1,6 @@
 import java.math.BigInteger;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 public class PartitionFunctionV2 {
 	public static BigInteger[][] pnmvals;
 	
@@ -21,9 +23,14 @@ public class PartitionFunctionV2 {
 	}
 	
 	public static void main(String[]args) {
-		pnmvals = new BigInteger[10001][10001];
-		for (int i = 0; i <= 10000; i++) {
+		LocalTime startTime = LocalTime.now();
+		int max = 10000;
+		pnmvals = new BigInteger[max+1][max+1];
+		for (int i = 0; i <= max; i++) {
 			System.out.println(i + ": " + p(i));
 		}
+		LocalTime endTime = LocalTime.now();
+		long runTime = startTime.until(endTime, ChronoUnit.SECONDS);
+		System.out.println("Total runtime: " + runTime + " seconds");
 	}
 }
