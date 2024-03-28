c4ebafc06370f97d884ebee95024eff781b67938
lbadler <122937810+lbadler@users.noreply.github.com>
2024-03-28 16:12:19 -0400

    Update PartitionFunctionV2.java

--- PartitionFunctionV2.java
+++ PartitionFunctionV2.java
@@ -8,23 +8,24 @@ public class PartitionFunctionV2 {
 		return p(n, n);
 	}
 	
+	//Number of partitions of n with largest partition at most max
 	public static BigInteger p(int n, int max) {
-		if (n == 0)
+		if (n == 0) //there is always 1 way to partition 0, regardless of max
 			return BigInteger.ONE;
-		if (max == 0)
+		if (max == 0) //if n != 0, we cannot divide it since all partitions are necessarily >0
 			return BigInteger.ZERO;
-		max = Math.min(n, max);
-		if (pnmvals[n][max] != null)
+		max = Math.min(n, max); //no partition can be larger than n, so if max is larger, set it to be equal to n
+		if (pnmvals[n][max] != null) //check lookup table
 			return pnmvals[n][max];
-		BigInteger sum = p(n, max-1).add(p(n-max, max));
-		pnmvals[n][max] = sum;
+		BigInteger sum = p(n, max-1).add(p(n-max, max)); //either we add one max sized partition, or we move on to smaller partitions
+		pnmvals[n][max] = sum; //update lookup table
 		return sum;
 	}
 	
 	public static void main(String[]args) {
 		LocalTime startTime = LocalTime.now();
-		int max = 25000;
-		pnmvals = new BigInteger[max+1][max+1];
+		int max = 10000;
+		pnmvals = new BigInteger[max+1][max+1]; //initialize lookup table
 		for (int i = 0; i <= max; i++) {
 			System.out.println(i + ": " + p(i));
 		}
